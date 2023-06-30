5c4f9004826de65e379fe351922eea116792390d
Rubén Gázquez Rosales <rgazquez@seyte.com>
2023-06-30 07:25:23 +0200

    home skills 80% ready

--- src/pages/home/skills/home-skills.tsx
+++ src/pages/home/skills/home-skills.tsx
@@ -1,5 +1,27 @@
 import React from "react";
+import styles from "./home-skills.module.scss";
+import { skills } from "./skills";
 
 export default function HomeSkills(): JSX.Element {
-  return <h2>SKILLS</h2>;
+  return (
+    <div>
+      <h2 className={styles.homeskills_title}>SKILLS</h2>
+      <div className={styles.homeskills_items_container}>
+        {skills &&
+          skills.map((skill) => (
+            <div className={styles.homeskills_item}>
+              <span>{skill.name}</span>
+              <div className={styles.homeskills_bar_container}>
+                <div
+                  className={styles.homeskills_bar}
+                  style={{ width: `${skill.percentage}%` }}
+                >
+                  <span>{skill.percentage}%</span>
+                </div>
+              </div>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
 }
---
+++ src/pages/home/skills/skills.tsx
@@ -0,0 +1,71 @@
+export interface Skill {
+  name: string;
+  percentage: number;
+}
+
+export const skills: Skill[] = [
+  {
+    name: "Javascript Typescript",
+    percentage: 95,
+  },
+  {
+    name: "Node Nest",
+    percentage: 95,
+  },
+  {
+    name: "Angular",
+    percentage: 95,
+  },
+  {
+    name: "Git Github Gitlab",
+    percentage: 95,
+  },
+  {
+    name: "UI UX Figma",
+    percentage: 95,
+  },
+  {
+    name: "HTML CSS Sass",
+    percentage: 95,
+  },
+  {
+    name: "Bootstrap, Tailwind",
+    percentage: 95,
+  },
+  {
+    name: "Bootstrap",
+    percentage: 95,
+  },
+  {
+    name: "SQL",
+    percentage: 95,
+  },
+  {
+    name: "Jest",
+    percentage: 90,
+  },
+  {
+    name: "Websockets, MQTT",
+    percentage: 80,
+  },
+  {
+    name: "Wordpress Divi",
+    percentage: 80,
+  },
+  {
+    name: "Linux, SSH",
+    percentage: 70,
+  },
+  {
+    name: "Docker, Docker Compose",
+    percentage: 70,
+  },
+  {
+    name: "React, React Native",
+    percentage: 60,
+  },
+  {
+    name: "IoT, Arduino, ESP32, Raspberry-pi",
+    percentage: 50,
+  },
+];
